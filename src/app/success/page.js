09a502e6f6ca91e2fa50c2dbd6cd34cecd1eaaeb
mosamochi <dajayan30@gmail.com>
2023-07-09 16:47:04 -0500

    starting point

---
+++ src/app/success/page.js
@@ -0,0 +1,22 @@
+import "./success-styles.css"
+import Link from "next/link";
+
+export default function Success() {
+
+  return (
+    <>
+    <header><h1>Thank you for (fake) joining</h1></header>
+    <main>
+        
+        <section>
+        <h2>Your data has not been recorded but you are a rockstar!</h2>
+        <p> Thank you for clicking through this far!</p>
+        </section>
+        <Link href='/'><img src="/kitty.jpg"></img></Link>
+        
+    </main>
+    </>
+   
+  );
+      }
+      
